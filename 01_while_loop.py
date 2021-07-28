06eb0df486fe777918104ecc40d18d58d01b0368
Ali Neaz <neazali77@gmail.com>
2021-07-29 02:17:13 +0600

    solved the quick quize printing 0 to 100 number using while loop in python

---
+++ 01_while_loop.py
@@ -0,0 +1,13 @@
+# 5:16
+
+i = 0
+while i < 10:
+    print("yes", i)
+    i = i+1
+
+# if condition is always true then the loop will be infinity
+
+
+a = 50
+while a > 50:
+    print(a)
---
+++ 02_quick_quize.py
@@ -0,0 +1,28 @@
+
+
+# # print zero to 100
+
+# i = 0
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+
+# # printing the even number
+# i = 0
+# while i <= 10:
+#     print(i)
+#     i += 2
+
+
+# # printing the odd number
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i += 2
+
+
+i = 0
+while i <= 100000:
+    print(i)
+    i += 2
